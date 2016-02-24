3a2b5cb5e02db82967b724d027bfed9a5363fa99
ghop1073 <gagehopkins1@gmail.com>
2016-02-24 16:08:23 -0700

    created new project

---
+++ src/Controller/NodeController.cpp
@@ -0,0 +1,37 @@
+/*
+ * NodeController.cpp
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: ghop1073
+ */
+#include "NodeController.h"
+#include <iostream>
+using namespace std;
+
+NodeController :: NodeController()
+{
+
+notHipsterInts = new CTECArray<int>(5);
+}
+
+NodeController :: ~NodeController()
+{
+
+}
+
+
+
+void NodeController :: start()
+{
+	for(int index = 0; index < notHipsterInts->getSize(); index++)
+	{
+		notHipsterInts->set(index, (index *23));
+	}
+
+	for(int index = notHipsterInts -> getSize() -  1; index >=0; index --)
+	{
+		cout << "the contents of not hipster ints array node " << index << " are " << notHipsterInts->get(index) << endl;
+	}
+
+
+}
---
+++ src/Controller/NodeController.h
@@ -0,0 +1,35 @@
+/*
+ * NodeController.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: ghop1073
+ */
+
+#ifndef CONTROLLER_NODECONTROLLER_H_
+#define CONTROLLER_NODECONTROLLER_H_
+#include "../Model/Node.h"
+#include "../Model/Node.cpp"
+#include "../Model/ArrayNode.cpp"
+#include "../Model/ArrayNode.h"
+#include "../Model/CTECArray.h"
+#include "../Model/CTECArray.cpp"
+
+
+#include <string>
+using namespace std;
+
+class NodeController
+{
+private:
+
+	CTECArray<int> * notHipsterInts;
+
+public:
+	virtual ~NodeController();
+	NodeController();
+	void start();
+};
+
+
+
+#endif /* CONTROLLER_NODECONTROLLER_H_ */
