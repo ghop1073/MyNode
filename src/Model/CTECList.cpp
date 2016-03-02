9bb18e23834acc34f35acb41334908de15226730
ghop1073 <gagehopkins1@gmail.com>
2016-03-02 11:49:08 -0700

    CTECList basic framework added to project

---
+++ src/Model/CTECList.cpp
@@ -0,0 +1,51 @@
+/*
+ * CTECList.h
+ *
+ *  Created on: Feb 29, 2016
+ *      Author: mhop1073
+ */
+
+#include "CTECList.h"
+#include <iostream>
+template <class Type>
+
+CTECList<Type>::CTECList()
+{
+
+}
+
+template<class Type>
+CTECList<Type>::~CTECList()
+{
+
+}
+
+template<class Type>
+void CTECList<Type>:: addAtIndex(int index, const Type& value)
+{
+
+}
+
+template<class Type>
+void CTECList<Type>:: addToEnd(const Type& value)
+{
+
+}
+
+template<class Type>
+void CTECList<Type>:: addToFront(const type& value)
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::removeFromFront()
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::removeFromPosition(int index)
+{
+
+}
--- src/Model/CTECList.h
+++ src/Model/CTECList.h
@@ -5,6 +5,16 @@
  *      Author: mhop1073
  */
 
+#ifndef MODEL_CTECLIST_H_
+#define MODEL_CTECLIST_H_
+#include "ArrayNode.h"
 
+template<class Type>
+class CTECList
+{
+
+};
+
+#endif /* MODEL_CTECLIST_H_ */
 
 
