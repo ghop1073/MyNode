f0a83026b15839eb17ecab4a837ae779d52c8fca
ghop1073 <gagehopkins1@gmail.com>
2016-03-02 12:27:27 -0700

    updated my entire Timer.h file in project

--- src/Model/Timer.h
+++ src/Model/Timer.h
@@ -5,6 +5,28 @@
  *      Author: mhop1073
  */
 
+#ifndef MODEL_TIMER_H_
+#define MODEL_TIMER_H_
 
+#include <time.h>
+
+class Timer
+{
+private:
+	clock_t executionTime;
+public:
+
+	Timer();
+	virtual ~Timer();
+	void startTimer();
+	void stopTimer();
+	void resetTimer();
+	void displayTimerInfo();
+	long getExecutionInMicorseconds();
+};
+
+
+
+#endif /* MODEL_TIMER_H_ */
 
 
